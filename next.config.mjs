30a02e1f3b0600f8f09481ae5b04293c00b9b394
Tanya Ivato <tetiana.ivato@gmail.com>
2024-07-29 17:25:16 +0300

    Updated yaml

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'dist',
   basePath: '/next-typescript-auth',
-  // output: 'export', // <=== enables static exports
+  output: 'export', // <=== enables static exports
   reactStrictMode: true,
 };
 
